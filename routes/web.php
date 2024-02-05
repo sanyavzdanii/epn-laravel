affcd295d01a28381dbf2b18f9161d9bcefa97d7
Alex <avzproject@gmail.com>
2024-02-05 18:39:09 +0200

    add scroll loading products

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/epn/goods/{page}', [EPNController::class, 'getEPNGoods']);
+Route::get('/api/epn/goods/{page}', [EPNController::class, 'getEPNGoods']);
